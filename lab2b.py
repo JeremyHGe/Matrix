2c6322f26d5629fd1fd97d0122fc021966661b21
Jeremy Hernandez <jeremyhg1006@gmail.com>
2026-05-14 17:36:41 +0000

    Finishing up

---
+++ lab2b.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+name = input("Enter your name: ")
+age = input("Enter your age")
+print('Hi'+name'you are'+age'years old.')
+
---
+++ python3/lab2b.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+print('Name: '+name+ '\nAge: '+age+  '\nHi ' + name + ', you are ' + age + ' years old.\n')
+
