bbd3ab46c7dc254e173b68673989fa8dfba25238
Jeremy Hernandez <jeremyhg1006@gmail.com>
2026-06-04 19:35:22 +0000

    Updating all labs so far: lab5a, lab5b, and CheckLab5. Also moving lab4a to the root of python3 since it is not part of lab5.

---
+++ python3/Lab5/lab5a.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# Author ID: 178127239
+# Author Name: Jeremy Hernandez
+
+def read_file_string(file_name):
+    f = open(file_name, 'r')
+    return f.read()
+
+def read_file_list(file_name):
+    # Takes a file_name as string for a file name, 
+    # return its entire contents as a list of lines without new-line characters
+    f = open(file_name, 'r')
+    return list(line.strip() for line in f.readlines())
+if __name__ == '__main__':
+    file_name = 'data.txt'
+    print(read_file_string(file_name))
+    print(read_file_list(file_name))
